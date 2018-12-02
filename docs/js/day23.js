6ca68336c4a995e5e8f9fa7178bc3ee1b589d5dc
alicegherbison <alicegherbison@gmail.com>
2018-12-02 16:43:28 +0000

    Regenerate docs folder

---
+++ docs/js/day23.js
@@ -0,0 +1,46 @@
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+  .filter(voice => (voice.name.includes('Daniel') || (voice.name.includes('Yuna'))))
+  .map((voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</options>`))
+  .reverse()
+  .join('');
+}
+
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === this.value)
+  toggle();
+}
+
+function toggle(startOver = true){
+  speechSynthesis.cancel();
+  if(startOver){
+    speechSynthesis.speak(msg);
+  }
+}
+
+function setOption(){
+  msg[this.name] = this.value;
+  toggle();
+}
+
+function setDefault(){
+  msg.lang = 'ko-KR';
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false));
+
+setDefault();
