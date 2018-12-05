a70bff8998ca88cb516cea218b3dbfaeefe31058
alicegherbison <alicegherbison@gmail.com>
2018-12-05 21:44:30 +0000

    Regenerate docs folder

---
+++ docs/js/day25.js
@@ -0,0 +1,12 @@
+document.body.classList.add('bod');
+
+const divs = document.querySelectorAll('div');
+
+function logText(e) {
+  console.log(this.classList.value);
+  e.stopPropagation();
+}
+
+divs.forEach(div => div.addEventListener('click', logText, {
+  capture: true 
+}));
