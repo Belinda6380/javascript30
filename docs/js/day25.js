41e0ebdb03b3b19ef54c1368810670071cc0cf4f
alicegherbison <alicegherbison@gmail.com>
2018-12-06 07:38:54 +0000

    Regenerate docs folder

--- docs/js/day25.js
+++ docs/js/day25.js
@@ -4,9 +4,9 @@ const divs = document.querySelectorAll('div');
 
 function logText(e) {
   console.log(this.classList.value);
-  e.stopPropagation();
 }
 
 divs.forEach(div => div.addEventListener('click', logText, {
-  capture: true 
+  capture: false,
+  once: true 
 }));
