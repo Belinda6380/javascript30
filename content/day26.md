e3680dfc9bcf8ffa0cc851d9f4fe51eda8cdfd3a
alicegherbison <alicegherbison@gmail.com>
2018-12-07 22:31:17 +0000

    Further work on day 26

--- content/day26.md
+++ content/day26.md
@@ -5,3 +5,5 @@ layout = "single"
 day = "26"
 screenshot = ""
 +++
+
+* That you can use a full CSS selector in {{< code "querySelector()" >}} methods (with child or sibling selectors)
---
+++ static/js/day26.js
@@ -0,0 +1,5 @@
+const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('.top');
+
+// 4.37
