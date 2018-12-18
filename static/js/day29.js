8182a4572acaca692f088e288d3df026fbe71db1
alicegherbison <alicegherbison@gmail.com>
2018-12-18 22:28:31 +0000

    Continue day 29

--- static/js/day29.js
+++ static/js/day29.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
   
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -26,6 +28,9 @@ function displayTimeLeft(seconds) {
 
 function displayEndTime(timestamp) {
   const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be back at ${hour}:${minutes}`;
 }
 
-// 12:38
+// 15:35
