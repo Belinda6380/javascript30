1b2994b4e33638dc34ac3e7a46606741b25c4d5b
alicegherbison <alicegherbison@gmail.com>
2018-12-19 18:41:43 +0000

    Reach end of day 28 video

--- content/day29.md
+++ content/day29.md
@@ -9,3 +9,7 @@ screenshot = ""
 * {{< code "Date.now()" >}} and {{< code "clearInterval()" >}} methods
 * Use {{< code "document.title()" >}} to update the title tag, which displays content in the tab top in the browser window
 * Passing the number of milliseconds past 1st January 1970) when to a new {{< code "Date" >}} object gives you a formatted timestamp
+* How to access custom data attributes on elements using {{< code "dataset" >}} property
+* How to use {{< code "clearInterval()" >}} to reset any existing timers
+* If an HTML element has a {{< code "name" >}} attribute, you can use dot notation to select it immediately without using {{< code "docuement.querySelector" >}} etc, and further dots can be used to grab any named nested elements within
+* The {{< code "reset()" >}} method
--- static/js/day29.js
+++ static/js/day29.js
@@ -1,8 +1,10 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+  clearInterval(countdown);
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
@@ -30,7 +32,18 @@ function displayEndTime(timestamp) {
   const end = new Date(timestamp);
   const hour = end.getHours();
   const minutes = end.getMinutes();
-  endTime.textContent = `Be back at ${hour}:${minutes}`;
+  endTime.textContent = `Be back at ${hour}:${minutes < 10? '0' : ''}${minutes}`;
 }
 
-// 15:35
+function startTimer(){
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const mins = this.minutes.value;
+  timer(mins * 60);
+  this.reset();
+});
